897622a19ed2593ff29f755cad13efe33834e8aa
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-16 10:48:06 -0600

    Adds rspec test for laugh track app, but it fails

--- spec/controllers/laugh_track_app_spec.rb
+++ spec/controllers/laugh_track_app_spec.rb
@@ -1,1 +1,10 @@
-RSpec.
+describe '.laugh_track_app' do
+  describe 'goes to comedian index' do
+    it 'shows comedians and their ages' do
+
+    visit '/comedians'
+
+    expect(page.body.h1).to include('Comedic Heroes and Heroines')
+    end
+  end
+end
