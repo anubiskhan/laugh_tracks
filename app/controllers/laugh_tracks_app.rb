e59a49fbdf649ed3dd2a952714a81987df97a90a
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-16 10:54:13 -0600

    Adds specials to /comedians

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -6,6 +6,7 @@ class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
     @comedians = Comedian.all
+    @specials = Special.all
 
     erb :'comedians/index'
   end
--- spec/controllers/laugh_track_app_spec.rb
+++ spec/controllers/laugh_track_app_spec.rb
@@ -4,7 +4,7 @@ describe '.laugh_track_app' do
 
     visit '/comedians'
 
-    expect(page.body.h1).to include('Comedic Heroes and Heroines')
+    expect(page.body).to include('Comedic Heroes and Heroines')
     end
   end
 end
